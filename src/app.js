53663e081fa8b33ce63dfd4c6f56b5cf02c48858
Weyslogya <weyslogya@hotmail.com>
2023-04-19 17:35:25 +0200

    js

---
+++ src/app.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded",(e)=>{
+    console.log(e.target)
+    console.log("Hello world")
+    let x = Number(0);
+    let y = 1;
+    let vide;
+    let message = String("Hello World")
+    let bool = true
+    const faux = false
+    console.log(typeof message)
+    let nom, prénom, ville
+    ville = "Paris"
+    nom == undefined ? alert("Error") : false
+    console.log(nom)
+    const table = ["html","css","js", true]
+    console.log(table.length)
+    console.table(table)
+    console.dir(table)
+})
